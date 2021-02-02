967d7004c481f88172f4e4b76b8c6f89f119c0ea
eharn <har09008@byui.edu>
2021-02-02 10:34:30 -0700

    images

--- Presentation/js/imageload.js
+++ Presentation/js/imageload.js
@@ -1,3 +1,5 @@
+
+
 let imagesToLoad = document.querySelectorAll('img[data-src]');
 const loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
@@ -8,21 +10,3 @@ const loadImages = (image) => {
 imagesToLoad.forEach((img) => {
     loadImages(img);
   });
-
-  if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-      items.forEach((item) => {
-        if(item.isIntersecting) {
-          loadImages(item.target);
-          observer.unobserve(item.target);
-        }
-      });
-    });
-    imagesToLoad.forEach((img) => {
-      observer.observe(img);
-    });
-  } else {
-    imagesToLoad.forEach((img) => {
-      loadImages(img);
-    });
-  }
