09ff4cb152db3a8d5ebc617a380e2c157810f372
eharn <har09008@byui.edu>
2021-02-11 10:06:20 -0700

    temp

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -3,7 +3,8 @@ var s = parseFloat(document.getElementById('speed').innerHTML);
 var f;
 
 if (t <= 50 && s > 3){
-    f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + .04275 * t * Math.pow(s,0.16);
+    f = 35.74 + 0.6215 * t - (35.75 * Math.pow(s,0.16)) + (0.4275 * t * Math.pow(s,0.16));
+    console.log(f);
     f = Math.round(f);
 }
 else {
