c648adfdc923a7bfade8a0c0c9c8d7dc633e01ab
eharn <har09008@byui.edu>
2021-02-10 18:02:57 -0700

    calc

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,8 +1,11 @@
-var t = document.getElementById("high").text;
-var s = document.getElementById("speed");
-var wind = 12;
+var t = parseFloat(document.getElementById('high').innerHTML);
+var s = parseFloat(document.getElementById('speed').innerHTML);
+var f;
 
+if (t <= 50 && s > 3){
+    f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + .04275 * t * Math.pow(s,0.16);
+}
 
-document.getElementById("wind").innerHTML = high;
+document.getElementById("wind").textContent = Math.round(f);
 
 
