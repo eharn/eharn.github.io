756bfac2758b3d9f1642db8cc35442b21dd22ccb
eharn <har09008@byui.edu>
2021-02-10 21:19:53 -0700

    temp

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -4,8 +4,12 @@ var f;
 
 if (t <= 50 && s > 3){
     f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + .04275 * t * Math.pow(s,0.16);
+    f = Math.round(f);
+}
+else {
+    f = 'N/A'
 }
 
-document.getElementById("wind").textContent = Math.round(f);
+document.getElementById("wind").textContent = f;
 
 
