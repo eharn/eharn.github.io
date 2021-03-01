06f8288a9c33a4c74a27032724e8ad5cef8bbdfd
eharn <har09008@byui.edu>
2021-03-01 10:09:37 -0700

    lazy load fix

--- lesson7/js/script.js
+++ lesson7/js/script.js
@@ -1,7 +1,7 @@
 const menunav = document.querySelector('.menu');
 const mainnav = document.querySelector('.navbar')
 
-menunav.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
 
 let imagesToLoad = document.querySelectorAll('img[data-src]');
 const loadOptions = {
