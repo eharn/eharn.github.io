992e6c1e963277dbb794e33dfcddfedd21d483d4
eharn <har09008@byui.edu>
2021-03-09 14:52:13 -0700

    lesson10remake

---
+++ lesson10/js/weatherapi2.js
@@ -0,0 +1,29 @@
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?q=Preston,5604473&units=imperial&appid=e5da55b917c0943f98ce55dedb68b5e8";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('humidity').textContent = jsObject.main.humidity;
+    document.getElementById('temp_max').textContent = Math.round(jsObject.main.temp_max);
+    document.getElementById('speed').textContent = Math.round(jsObject.wind.speed);
+    const desc = jsObject.weather[0].description; 
+    document.getElementById('currently').textContent = desc;
+  });
+
+  const forcast = "http://api.openweathermap.org/data/2.5/forecast?q=Preston,5604473&units=imperial&appid=8b6ec3fb6e417dc46bee5c8dcafdab72";
+  fetch(forcast)
+  .then((response) => response.json())
+  .then((jsObject) => {
+  console.log(jsObject);
+  var list = jsObject.list;
+  var days = ['mon', 'tue', 'wed', 'thurs', 'fri'];
+  var counter = 0;
+       
+  for (var i = 0 ; i < list.length; i++){
+        if (list[i].dt_txt.includes("18:00:00")) {
+            document.getElementById(days[counter]).textContent = Math.round(list[i].main.temp);
+            console.log(days[counter]);
+            counter++;
+        }
+  }
+});
---
+++ lesson10/js/windchill.js
@@ -0,0 +1,16 @@
+var t = parseFloat(document.getElementById('temp_max').innerHTML);
+var s = parseFloat(document.getElementById('speed').innerHTML);
+var f;
+
+if (t <= 50 && s > 3){
+    f = 35.74 + 0.6215 * t - (35.75 * Math.pow(s,0.16)) + (0.4275 * t * Math.pow(s,0.16));
+    console.log(f);
+    f = Math.round(f);
+}
+else {
+    f = 'N/A'
+}
+
+document.getElementById("wind").textContent = f;
+
+
