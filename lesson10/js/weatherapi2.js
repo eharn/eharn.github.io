4556e2cf1ddf12a4e44e06345ea41964abede76b
eharn <har09008@byui.edu>
2021-03-10 11:57:07 -0700

    https

--- lesson10/js/weatherapi2.js
+++ lesson10/js/weatherapi2.js
@@ -1,4 +1,4 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?q=Preston,5604473&units=imperial&appid=e5da55b917c0943f98ce55dedb68b5e8";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Preston,5604473&units=imperial&appid=e5da55b917c0943f98ce55dedb68b5e8";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
@@ -10,7 +10,7 @@ fetch(apiURL)
     document.getElementById('currently').textContent = desc;
   });
 
-  const forcast = "http://api.openweathermap.org/data/2.5/forecast?q=Preston,5604473&units=imperial&appid=8b6ec3fb6e417dc46bee5c8dcafdab72";
+  const forcast = "https://api.openweathermap.org/data/2.5/forecast?q=Preston,5604473&units=imperial&appid=8b6ec3fb6e417dc46bee5c8dcafdab72";
   fetch(forcast)
   .then((response) => response.json())
   .then((jsObject) => {
