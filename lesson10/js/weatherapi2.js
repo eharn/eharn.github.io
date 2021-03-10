3963607c8cce15fcf54dc2f137b82661d8f56031
eharn <har09008@byui.edu>
2021-03-10 13:41:04 -0700

    days

--- lesson10/js/weatherapi2.js
+++ lesson10/js/weatherapi2.js
@@ -8,7 +8,7 @@ fetch(apiURL)
     document.getElementById('speed').textContent = Math.round(jsObject.wind.speed);
     const desc = jsObject.weather[0].description; 
     document.getElementById('currently').textContent = desc;
-  });
+
 
   const forcast = "https://api.openweathermap.org/data/2.5/forecast?q=Preston,5604473&units=imperial&appid=8b6ec3fb6e417dc46bee5c8dcafdab72";
   fetch(forcast)
@@ -18,12 +18,38 @@ fetch(apiURL)
   var list = jsObject.list;
   var days = ['mon', 'tue', 'wed', 'thurs', 'fri'];
   var counter = 0;
+
+  var iconday = ['m', 't', 'w', 'th', 'f'];
+  var countericon = 0;
+  var counter = 0;
+  var header = ['day1', 'day2', 'day3', 'day4', 'day5'];
+ 
        
   for (var i = 0 ; i < list.length; i++){
         if (list[i].dt_txt.includes("18:00:00")) {
             document.getElementById(days[counter]).textContent = Math.round(list[i].main.temp) + ' °F';
+            var icon = "http://openweathermap.org/img/wn/" + list[i].weather[0].icon + "@2x.png";
+            document.getElementById(iconday[countericon]).setAttribute('src', icon);
+
+
+            var dt = jsObject.list[i].dt;
+  
+            function dayHeader(dt){
+              let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thurs', 'Fri', 'Sat'];
+              var time = dt * 1000;
+              let date = new Date(time);
+              return days[date.getDay()];
+            }
+
+            document.getElementById(header[counter]).textContent = dayHeader(dt);
+
             console.log(days[counter]);
             counter++;
+            countericon++;
         }
   }
+
+
+
+});
 });
